36733b99dec609b07fdf3295224e893bb2cf100f
Leo Romanovsky <leo@geteppo.com>
2024-10-14 21:58:27 -0700

    config test

--- python-sdk/tests/test_configuration.py
+++ python-sdk/tests/test_configuration.py
@@ -11,52 +11,87 @@ FLAGS_CONFIG = json.dumps({
     "flags": {}
 }).encode('utf-8')
 
-BANDITS_CONFIG = json.dumps({
-        "updatedAt": "2023-09-13T04:52:06.462Z",
-        "environment": {
-            "name": "Test"
-        },
-        "bandits": {
-            "car_bandit": {
-                "banditKey": "car_bandit",
-                "modelName": "falcon",
-                "updatedAt": "2023-09-13T04:52:06.462Z",
-                "modelVersion": "v456",
-                "modelData": {
-                    "gamma": 1.0,
-                    "defaultActionScore": 5.0,
-                    "actionProbabilityFloor": 0.2,
-                    "coefficients": {
-                        "toyota": {
-                            "actionKey": "toyota",
-                            "intercept": 1.0,
-                            "actionNumericCoefficients": [{
-                                "attributeKey": "speed",
-                                "coefficient": 1,
-                                "missingValueCoefficient": 0.0
-                            }],
-                            "actionCategoricalCoefficients": [],
-                            "subjectNumericCoefficients": [],
-                            "subjectCategoricalCoefficients": []
-                        }
+FLAGS_CONFIG_WITH_BANDITS = ufc_with_bandits = json.dumps({
+    "createdAt": "2024-09-09T10:18:15.988Z",
+    "environment": {"name": "test"},
+    "flags": {},
+    "bandits": {
+        "banner_bandit": [
+            {
+                "key": "banner_bandit",
+                "flagKey": "banner_bandit_flag",
+                "variationKey": "banner_bandit",
+                "variationValue": "banner_bandit"
+            },
+            {
+                "key": "banner_bandit",
+                "flagKey": "banner_bandit_flag_uk_only",
+                "variationKey": "banner_bandit",
+                "variationValue": "banner_bandit"
+            }
+        ],
+        "car_bandit": [
+            {
+                "key": "car_bandit",
+                "flagKey": "car_bandit_flag",
+                "variationKey": "car_bandit",
+                "variationValue": "car_bandit"
+            }
+        ],
+    }
+}).encode('utf-8')
+
+BANDITS_MODEL_CONFIG = json.dumps({
+    "updatedAt": "2023-09-13T04:52:06.462Z",
+    "environment": {
+        "name": "Test"
+    },
+    "bandits": {
+        "car_bandit": {
+            "banditKey": "car_bandit",
+            "modelName": "falcon",
+            "updatedAt": "2023-09-13T04:52:06.462Z",
+            "modelVersion": "v456",
+            "modelData": {
+                "gamma": 1.0,
+                "defaultActionScore": 5.0,
+                "actionProbabilityFloor": 0.2,
+                "coefficients": {
+                    "toyota": {
+                        "actionKey": "toyota",
+                        "intercept": 1.0,
+                        "actionNumericCoefficients": [{
+                            "attributeKey": "speed",
+                            "coefficient": 1,
+                            "missingValueCoefficient": 0.0
+                        }],
+                        "actionCategoricalCoefficients": [],
+                        "subjectNumericCoefficients": [],
+                        "subjectCategoricalCoefficients": []
                     }
                 }
             }
         }
-    }).encode('utf-8')
+    }
+}).encode('utf-8')
 
 class TestConfiguration:
     def test_init_valid(self):
         Configuration(flags_configuration=FLAGS_CONFIG)
 
-    def test_bandit_configuration():
-        # Initialize Configuration with both flags and bandits
-        config = Configuration(flags_configuration=FLAGS_CONFIG, bandits_configuration=BANDITS_CONFIG)
+    def test_bandit_configuration(self):
+        config = Configuration(flags_configuration=FLAGS_CONFIG_WITH_BANDITS)
 
         # Call get_bandit_keys and check the output
         bandit_keys = config.get_bandit_keys()
         assert isinstance(bandit_keys, set)
-        assert bandit_keys == {"car_bandit"}
+        assert bandit_keys == {"banner_bandit", "car_bandit"}
+
+    def test_bandit_model_configuration(self):
+        config = Configuration(flags_configuration=FLAGS_CONFIG_WITH_BANDITS, bandits_configuration=BANDITS_MODEL_CONFIG)
+        bandit_keys = config.get_bandit_keys()
+        assert isinstance(bandit_keys, set)
+        assert bandit_keys == {"banner_bandit", "car_bandit"}
 
     def test_init_invalid_json(self):
         """Input is not valid JSON string."""
@@ -85,18 +120,5 @@ def test_configuration_some():
     flag_config = configuration.get_flags_configuration()
 
     result = json.loads(flag_config)
-    assert result["environment"] == {"name": "Test"}
-    assert "numeric_flag" in result["flags"]
-
-
-@pytest.mark.rust_only
-def test_configuration_bandits_only():
-    client = init("bandits", wait_for_init=True)
-    configuration = client.get_configuration()
-    assert configuration != None
-
-    bandit_config = configuration.get_bandit_keys()
-
-    result = json.loads(bandit_config)
     assert result["environment"] == {"name": "Test"}
     assert "numeric_flag" in result["flags"]
