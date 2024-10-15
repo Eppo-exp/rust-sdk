4636ace4d5461919596fc0de544f713075798cc6
Oleksii Shmalko <oleksii@fluxon.com>
2024-10-15 13:26:26 +0300

    refactor: remove unused double-assignment

--- python-sdk/tests/test_configuration.py
+++ python-sdk/tests/test_configuration.py
@@ -5,75 +5,82 @@ import json
 
 from .util import init
 
-FLAGS_CONFIG = json.dumps({
-    "createdAt": "2024-09-09T10:18:15.988Z",
-    "environment": {"name": "test"},
-    "flags": {}
-}).encode('utf-8')
-
-FLAGS_CONFIG_WITH_BANDITS = ufc_with_bandits = json.dumps({
-    "createdAt": "2024-09-09T10:18:15.988Z",
-    "environment": {"name": "test"},
-    "flags": {},
-    "bandits": {
-        "banner_bandit": [
-            {
-                "key": "banner_bandit",
-                "flagKey": "banner_bandit_flag",
-                "variationKey": "banner_bandit",
-                "variationValue": "banner_bandit"
-            },
-            {
-                "key": "banner_bandit",
-                "flagKey": "banner_bandit_flag_uk_only",
-                "variationKey": "banner_bandit",
-                "variationValue": "banner_bandit"
-            }
-        ],
-        "car_bandit": [
-            {
-                "key": "car_bandit",
-                "flagKey": "car_bandit_flag",
-                "variationKey": "car_bandit",
-                "variationValue": "car_bandit"
-            }
-        ],
+FLAGS_CONFIG = json.dumps(
+    {
+        "createdAt": "2024-09-09T10:18:15.988Z",
+        "environment": {"name": "test"},
+        "flags": {},
     }
-}).encode('utf-8')
+).encode("utf-8")
 
-BANDITS_MODEL_CONFIG = json.dumps({
-    "updatedAt": "2023-09-13T04:52:06.462Z",
-    "environment": {
-        "name": "Test"
-    },
-    "bandits": {
-        "car_bandit": {
-            "banditKey": "car_bandit",
-            "modelName": "falcon",
-            "updatedAt": "2023-09-13T04:52:06.462Z",
-            "modelVersion": "v456",
-            "modelData": {
-                "gamma": 1.0,
-                "defaultActionScore": 5.0,
-                "actionProbabilityFloor": 0.2,
-                "coefficients": {
-                    "toyota": {
-                        "actionKey": "toyota",
-                        "intercept": 1.0,
-                        "actionNumericCoefficients": [{
-                            "attributeKey": "speed",
-                            "coefficient": 1,
-                            "missingValueCoefficient": 0.0
-                        }],
-                        "actionCategoricalCoefficients": [],
-                        "subjectNumericCoefficients": [],
-                        "subjectCategoricalCoefficients": []
-                    }
+FLAGS_CONFIG_WITH_BANDITS = json.dumps(
+    {
+        "createdAt": "2024-09-09T10:18:15.988Z",
+        "environment": {"name": "test"},
+        "flags": {},
+        "bandits": {
+            "banner_bandit": [
+                {
+                    "key": "banner_bandit",
+                    "flagKey": "banner_bandit_flag",
+                    "variationKey": "banner_bandit",
+                    "variationValue": "banner_bandit",
+                },
+                {
+                    "key": "banner_bandit",
+                    "flagKey": "banner_bandit_flag_uk_only",
+                    "variationKey": "banner_bandit",
+                    "variationValue": "banner_bandit",
+                },
+            ],
+            "car_bandit": [
+                {
+                    "key": "car_bandit",
+                    "flagKey": "car_bandit_flag",
+                    "variationKey": "car_bandit",
+                    "variationValue": "car_bandit",
                 }
-            }
-        }
+            ],
+        },
     }
-}).encode('utf-8')
+).encode("utf-8")
+
+BANDITS_MODEL_CONFIG = json.dumps(
+    {
+        "updatedAt": "2023-09-13T04:52:06.462Z",
+        "environment": {"name": "Test"},
+        "bandits": {
+            "car_bandit": {
+                "banditKey": "car_bandit",
+                "modelName": "falcon",
+                "updatedAt": "2023-09-13T04:52:06.462Z",
+                "modelVersion": "v456",
+                "modelData": {
+                    "gamma": 1.0,
+                    "defaultActionScore": 5.0,
+                    "actionProbabilityFloor": 0.2,
+                    "coefficients": {
+                        "toyota": {
+                            "actionKey": "toyota",
+                            "intercept": 1.0,
+                            "actionNumericCoefficients": [
+                                {
+                                    "attributeKey": "speed",
+                                    "coefficient": 1,
+                                    "missingValueCoefficient": 0.0,
+                                }
+                            ],
+                            "actionCategoricalCoefficients": [],
+                            "subjectNumericCoefficients": [],
+                            "subjectCategoricalCoefficients": [],
+                        }
+                    },
+                },
+            }
+        },
+    }
+).encode("utf-8")
+
 
 class TestConfiguration:
     def test_init_valid(self):
@@ -88,7 +95,10 @@ class TestConfiguration:
         assert len(bandit_keys) == 0
 
     def test_bandit_model_configuration(self):
-        config = Configuration(flags_configuration=FLAGS_CONFIG_WITH_BANDITS, bandits_configuration=BANDITS_MODEL_CONFIG)
+        config = Configuration(
+            flags_configuration=FLAGS_CONFIG_WITH_BANDITS,
+            bandits_configuration=BANDITS_MODEL_CONFIG,
+        )
         bandit_keys = config.get_bandit_keys()
         assert isinstance(bandit_keys, set)
         # `car_bandit` is the only bandit in the model config.
@@ -106,11 +116,13 @@ class TestConfiguration:
                 flags_configuration=b'{"createdAt":"2024-09-09T10:18:15.988Z","environment":{"name":"test"},"flags":[]}'
             )
 
+
 @pytest.mark.rust_only
 def test_configuration_none():
     client = init("ufc", wait_for_init=False)
     configuration = client.get_configuration()
     assert configuration == None
+
 
 @pytest.mark.rust_only
 def test_configuration_some():
