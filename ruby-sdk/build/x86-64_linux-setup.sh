5d98d296769defdaa371b79ab950262022b54cde
Felipe Lima <felipe.lima@gmail.com>
2024-10-16 15:37:48 -0700

    fix(ruby): Enable ruby-sdk to be cross compiled ahead of time (#42)
    * fix(ruby): Enable rust-sdk to be cross compiled ahead of time
    
    * add github action to cross compile gems
    
    * remove comment
    
    * fix indentation
    
    * change working directory for ruby
    
    * fix cross-gem agai
    
    * yet another fix
    
    * another try
    
    * yep
    
    * manually run cross compile
    
    * fix indentation
    
    * more fixes
    
    * skip tags
    
    * adapt build from https://github.com/BoundaryML/baml/blob/canary/.github/workflows/build-ruby-release.reusable.yaml
    
    * fix path
    
    * Fix https://github.com/openssl/openssl/pull/25367\#issuecomment-2338141250
    
    * chore: add "vendored" feature flag to eppo_core
    
    * chore(ruby): build gem for 3.0 version
    
    * code review comments
    
    * add fake publish step for testing
    
    * comment out needs
    
    * update cargo lockfile
    
    * fix file path
    
    * fix version
    
    * another try
    
    * another try
    
    * 20th time is the charm
    
    * fix paths
    
    * add ls for debugging
    
    * final try
    
    * move cross_gems and publish to publish.yml
    
    ---------
    
    Co-authored-by: Oleksii Shmalko <oleksii@fluxon.com>

---
+++ ruby-sdk/build/x86-64_linux-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## From:
+## https://github.com/BoundaryML/baml/blob/canary/engine/language_client_ruby/x86-64_linux-setup.sh
+##
+
+set -euxo pipefail
+
+# from https://serverfault.com/questions/1161816/mirrorlist-centos-org-no-longer-resolve
+sudo sed -i 's/mirror.centos.org/vault.centos.org/g' /etc/yum.repos.d/*.repo
+sudo sed -i 's/^#.*baseurl=http/baseurl=http/g'      /etc/yum.repos.d/*.repo
+sudo sed -i 's/^mirrorlist=http/#mirrorlist=http/g'  /etc/yum.repos.d/*.repo
+
+ls /etc/yum.repos.d/
+
+# We need this to build engine/, since it's needed for OpenSSL
+sudo yum install -y perl-IPC-Cmd
