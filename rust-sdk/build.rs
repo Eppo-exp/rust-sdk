a1ac5deb94d85ab1f51272c7769b48647ce9b211
Leo Romanovsky <leoromanovsky@gmail.com>
2024-11-09 00:32:41 -0800

    chore: generate rust-sdk github action with build.rs

---
+++ rust-sdk/build.rs
@@ -0,0 +1,47 @@
+use gh_workflow::*;
+use toolchain::Toolchain;
+
+fn main() {
+    let build = Job::new("Build and Test Rust SDK")
+        .add_step(Step::checkout())
+        .add_step(
+            Toolchain::default()
+                .add_stable()
+                .add_nightly()
+                .add_clippy()
+                .add_fmt(),
+        )
+        .add_step(
+            Cargo::new("test")
+                .args("--all-features --workspace")
+                .name("Cargo Test"),
+        )
+        .add_step(
+            Cargo::new("fmt")
+                .nightly()
+                .args("--check")
+                .name("Cargo Fmt"),
+        )
+        .add_step(
+            Cargo::new("clippy")
+                .nightly()
+                .args("--all-features --workspace -- -D warnings")
+                .name("Cargo Clippy"),
+        );
+
+    let event = Event::default()
+        .push(Push::default().add_branch("main"))
+        .pull_request_target(
+            PullRequestTarget::default()
+                .open()
+                .synchronize()
+                .reopen()
+                .add_branch("main"),
+        );
+
+    Workflow::new("Build and Test")
+        .on(event)
+        .add_job("build", build)
+        .generate()
+        .unwrap();
+}
