d78c7d4ef4548c04e1ff66195bd02e1845ff42d9
Leo Romanovsky <leoromanovsky@gmail.com>
2024-11-11 21:15:50 -0800

    git submodules; npm ci; build and docs

--- rust-sdk/build.rs
+++ rust-sdk/build.rs
@@ -1,19 +1,24 @@
 use gh_workflow::*;
+use indexmap::IndexMap;
+use serde_json::json;
 use toolchain::Toolchain;
 
 fn main() {
+    let mut submodules_map = IndexMap::new();
+    submodules_map.insert("submodules".to_string(), json!("true"));
+
     let build = Job::new("Build and Test Rust SDK")
-        .add_step(Step::checkout())
+        .add_step(Step::checkout().add_with(submodules_map))
+        .add_step(Step::run("npm ci"))
+        .add_step(Toolchain::default().add_stable().add_clippy().add_fmt())
         .add_step(
-            Toolchain::default()
-                .add_stable()
-                .add_nightly()
-                .add_clippy()
-                .add_fmt(),
+            Cargo::new("build")
+                .args("--all-features --all-targets --verbose")
+                .name("Cargo Build"),
         )
         .add_step(
             Cargo::new("test")
-                .args("--all-features --workspace")
+                .args("--all-features --verbose")
                 .name("Cargo Test"),
         )
         .add_step(
@@ -27,7 +32,8 @@ fn main() {
                 .nightly()
                 .args("--all-features --workspace -- -D warnings")
                 .name("Cargo Clippy"),
-        );
+        )
+        .add_step(Cargo::new("doc").args("--verbose").name("Cargo Doc"));
 
     let event = Event::default()
         .push(Push::default().add_branch("main"))
