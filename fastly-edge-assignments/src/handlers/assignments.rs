48d2ebcc857b7a32876ac35c67d5ff058aea119f
Leo Romanovsky <leo@geteppo.com>
2024-11-18 13:51:12 -0800

    tidy up error logging

--- fastly-edge-assignments/src/handlers/assignments.rs
+++ fastly-edge-assignments/src/handlers/assignments.rs
@@ -13,9 +13,10 @@ use std::sync::Arc;
 struct RequestBody {
     subject_key: String,
     subject_attributes: Arc<Attributes>,
-    #[serde(rename = "banditActions")]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    bandit_actions: Option<HashMap<String, serde_json::Value>>,
+    // TODO: Add bandit actions
+    // #[serde(rename = "banditActions")]
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // bandit_actions: Option<HashMap<String, serde_json::Value>>,
 }
 
 #[derive(Debug, Serialize)]
@@ -25,64 +26,71 @@ struct AssignmentsResponse {
 }
 
 const KV_STORE_NAME: &str = "edge-assignment-kv-store";
-const SDK_KEY_QUERY_PARAM: &str = "sdk_key";
+const SDK_KEY_QUERY_PARAM: &str = "apiKey"; // For legacy reasons this is named `apiKey`
+const SDK_KEY_PREFIX_LENGTH: usize = 8;
 
-fn kv_store_key(api_key: &str) -> String {
-    format!("ufc-by-sdk-key-{}", api_key)
+const SDK_NAME: &str = "fastly-edge-assignments";
+const SDK_VERSION: &str = "0.1.0";
+
+fn kv_store_key(sdk_key_prefix: &str) -> String {
+    format!("ufc-by-sdk-key-prefix-{}", sdk_key_prefix)
 }
 
 pub fn handle_assignments(mut req: Request) -> Result<Response, Error> {
-    // Extract the API key first before we consume the request
-    let api_key = match req.get_query_parameter(SDK_KEY_QUERY_PARAM) {
-        Some(key) if !key.is_empty() => key.to_string(), // Convert to owned String
+    // Extract the SDK key prefix first before we consume the request
+    let sdk_key_prefix = match req.get_query_parameter(SDK_KEY_QUERY_PARAM) {
+        Some(key) if !key.is_empty() => key.chars().take(SDK_KEY_PREFIX_LENGTH).collect::<String>(),
         _ => {
-            return Ok(Response::from_status(StatusCode::BAD_REQUEST)
-                .with_body_text_plain("Missing required query parameter: sdk_key"));
-        }
-    };
-
-    // Now we can consume the request body
-    let body: RequestBody = match serde_json::from_slice::<RequestBody>(&req.take_body_bytes()) {
-        Ok(body) => {
-            if body.subject_key.is_empty() {
-                return Ok(Response::from_status(StatusCode::BAD_REQUEST)
-                    .with_body_text_plain("subject_key is required and cannot be empty"));
-            }
-            body
-        }
-        Err(e) => {
-            let error_message = if e.to_string().contains("subject_key") {
-                "subject_key is required in the request body"
-            } else {
-                "Invalid request body format"
-            };
             return Ok(
-                Response::from_status(StatusCode::BAD_REQUEST).with_body_text_plain(error_message)
+                Response::from_status(StatusCode::BAD_REQUEST).with_body_text_plain(&format!(
+                    "Missing required query parameter: {}",
+                    SDK_KEY_QUERY_PARAM
+                )),
             );
         }
     };
 
-    let subject_key = body.subject_key;
-    let subject_attributes = body.subject_attributes;
-    let bandit_actions = body.bandit_actions;
+    // Deserialize the request body into a struct
+    let (subject_key, subject_attributes): (eppo_core::Str, Arc<Attributes>) =
+        match serde_json::from_slice::<RequestBody>(&req.take_body_bytes()) {
+            Ok(body) => {
+                if body.subject_key.is_empty() {
+                    return Ok(Response::from_status(StatusCode::BAD_REQUEST)
+                        .with_body_text_plain("subject_key is required and cannot be empty"));
+                }
+                (
+                    eppo_core::Str::from(body.subject_key),
+                    body.subject_attributes,
+                )
+            }
+            Err(e) => {
+                let error_message = if e.to_string().contains("subject_key") {
+                    "subject_key is required in the request body"
+                } else {
+                    "Invalid request body format"
+                };
+                return Ok(Response::from_status(StatusCode::BAD_REQUEST)
+                    .with_body_text_plain(error_message));
+            }
+        };
 
-    // Construct an KVStore instance which is connected to the KV Store named `my-store`
-    // [Documentation for the KVStore open method can be found here](https://docs.rs/fastly/latest/fastly/struct.KVStore.html#method.open)
+    // Open the KV store
     let kv_store = KVStore::open(KV_STORE_NAME).map(|store| store.expect("KVStore exists"))?;
 
-    let mut kv_store_item = match kv_store.lookup(&kv_store_key(&api_key)) {
+    let mut kv_store_item = match kv_store.lookup(&kv_store_key(&sdk_key_prefix)) {
         Ok(item) => item,
         Err(e) => {
             let (status, message) = match e {
-                // Return unauthorized if the key does not exist.
-                // Our protocol lets the client know that the SDK key has not had a UFC
-                // configuration pre-computed for it in the KV Store.
-                KVStoreError::ItemNotFound => (
-                    StatusCode::UNAUTHORIZED,
-                    "SDK key not found in KV store".to_string(),
-                ),
+                KVStoreError::ItemNotFound => {
+                    eprintln!("Missing configuration for SDK key: {}", sdk_key_prefix);
+
+                    // Return unauthorized if the key does not exist.
+                    // Our protocol lets the client know that the SDK key has not had a UFC
+                    // configuration pre-computed for it in the KV Store.
+                    (StatusCode::UNAUTHORIZED, "Invalid SDK key.".to_string())
+                }
                 _ => {
-                    //fastly::log::error!("KV Store error: {:?}", e);
+                    eprintln!("KV Store error: {:?}", e);
                     (
                         StatusCode::INTERNAL_SERVER_ERROR,
                         "Unexpected KV Store error".to_string(),
@@ -98,53 +106,46 @@ pub fn handle_assignments(mut req: Request) -> Result<Response, Error> {
     let kv_store_item_body = kv_store_item.take_body();
     let ufc_config = match UniversalFlagConfig::from_json(
         SdkMetadata {
-            name: "fastly-edge-assignments",
-            version: "0.1.0",
+            name: SDK_NAME,
+            version: SDK_VERSION,
         },
         kv_store_item_body.into_bytes(),
     ) {
         Ok(config) => config,
         Err(e) => {
-            //fastly::log::error!("Failed to parse UFC config: {:?}", e);
+            eprintln!("Failed to parse UFC config: {:?}", e);
             return Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
                 .with_body_text_plain("Invalid configuration format in KV store"));
         }
     };
 
     let configuration = Configuration::from_server_response(ufc_config, None);
+    let flag_keys = configuration.flag_keys();
     let configuration_store = ConfigurationStore::new();
     configuration_store.set_configuration(Arc::new(configuration));
     let evaluator = Evaluator::new(EvaluatorConfig {
         configuration_store: Arc::new(configuration_store),
         sdk_metadata: SdkMetadata {
-            name: "fastly-edge-assignments",
-            version: "0.1.0",
+            name: SDK_NAME,
+            version: SDK_VERSION,
         },
     });
 
-    let subject_assignments = match evaluator
-        .get_subject_assignments(&eppo_core::Str::from(subject_key), &subject_attributes)
-        .into_iter()
-        .map(|(key, value)| value.map(|opt_assignment| (key, opt_assignment)))
-        .collect::<Result<Vec<_>, _>>()
-    {
-        Ok(assignments) => assignments
-            .into_iter()
-            .filter_map(|(key, opt_assignment)| {
-                opt_assignment.map(|assignment| (key, assignment.value))
-            })
-            .collect::<HashMap<_, _>>(),
-        Err(e) => {
-            // If we encounter an error in any of the assignments, return an internal server error.
-            //
-            // If any of the assignments produces an error during evaluation, the collection will short-circuit and return that first error.
-            // It won't continue processing the remaining assignments.
-            //fastly::log::error!("Assignment evaluation error: {:?}", e);
-            return Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
-                .with_body_text_plain(&format!("Failed to evaluate assignment: {}", e)));
-        }
-    };
+    let subject_assignments = flag_keys
+        .iter()
+        .filter_map(|key| {
+            match evaluator.get_assignment(key, &subject_key, &subject_attributes, None) {
+                Ok(Some(assignment)) => Some((key.clone(), assignment.value)),
+                Ok(None) => None,
+                Err(e) => {
+                    eprintln!("Failed to evaluate assignment for key {}: {:?}", key, e);
+                    None
+                }
+            }
+        })
+        .collect::<HashMap<_, _>>();
 
+    // Create the response
     let assignments_response = AssignmentsResponse {
         assignments: subject_assignments,
         timestamp: chrono::Utc::now().timestamp(),
@@ -155,7 +156,7 @@ pub fn handle_assignments(mut req: Request) -> Result<Response, Error> {
     {
         Ok(response) => response,
         Err(e) => {
-            // fastly::log::error!("Failed to serialize response: {:?}", e);
+            eprintln!("Failed to serialize response: {:?}", e);
             return Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
                 .with_body_text_plain("Failed to serialize response"));
         }
