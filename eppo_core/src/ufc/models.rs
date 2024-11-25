47bd195bc188ecc2dfed724af8f1b4a191e715d7
Leo Romanovsky <leo@geteppo.com>
2024-11-25 15:00:53 -0800

    Make UniversalFlagConfigWire.format optional

--- eppo_core/src/ufc/models.rs
+++ eppo_core/src/ufc/models.rs
@@ -18,7 +18,8 @@ pub type Timestamp = chrono::DateTime<chrono::Utc>;
 pub(crate) struct UniversalFlagConfigWire {
     /// When configuration was last updated.
     pub created_at: Timestamp,
-    pub format: AssignmentFormat,
+    #[serde(default)]
+    pub format: Option<AssignmentFormat>,
     /// Environment this configuration belongs to.
     pub environment: Environment,
     /// Flags configuration.
