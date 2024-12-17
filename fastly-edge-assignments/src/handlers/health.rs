1ae1fde730ce94a6e243806a27d4ac721052d492
Oleksii Shmalko <oleksii@fluxon.com>
2024-12-17 16:27:31 +0200

    chore: remove edge assignments service

--- fastly-edge-assignments/src/handlers/health.rs
+++
@@ -1,5 +0,0 @@
-use fastly::{http::StatusCode, Error, Request, Response};
-
-pub fn handle_health(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK).with_body_text_plain("OK"))
-}
